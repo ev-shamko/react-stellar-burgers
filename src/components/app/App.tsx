0cc82afbf6151fe907b2095cb2ed7bce69a2f6c1
ev-shamko <ev-shamko@yandex.ru>
2021-06-07 03:38:24 +0300

    changed App.tsx to app.tsx

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from '../../images/logo.svg';
-import indexStyles from './App.module.css';
+import indexStyles from './app.module.css';
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor'
