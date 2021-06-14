1a9e6e0d30b192f03d0b18707189015530318bbd
ev-shamko <ev-shamko@yandex.ru>
2021-06-14 06:01:34 +0300

    BurgerIngridients is now functional component

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,68 +1,50 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import { IngridientsListContext } from '../../services/ingridientsContext';
 
 import ingrStyles from "./burger-ingredients.module.css";
-//import IngridientCard from "../ingridient-card/ingrdient-card";
 import CardList from "../ingridients-cardlist/ingridients-cardlist";
+
 import {
     Tab,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 
 // <BurgerIngredients allIngridients={ingridientsData} openModal={openModal} /> 
 // @ts-ignore
-class BurgerIngredients extends React.Component {
+function BurgerIngredients ( {allIngridients, openModal } ) {
 
-    constructor(props) {
-        super(props);
+    // стейт для переключения активного таба в компоненте <Tab />
+    // компонент <Tab /> "под капотом" передаёт этому методу в качестве аргумента значение пропса value
+    const [currentTab, setCurrentTab] = React.useState('one');
 
-        this.state = {
-           currentTab: 'one',
-        }
-
-        this.tabSwitch = this.tabSwitch.bind(this);
-    }
-
-
-    // дока как это написать для функционального компонента: https://yandex-praktikum.github.io/react-developer-burger-ui-components/docs/tab 
-
-    tabSwitch(value) {
-        // console.log(value); // походу принимает не событие, а атрибут/пропс value компонтента <Tab />
-        this.setState({
-            ...this.state,
-            currentTab: value
-        });
-    }
-
-    render(props) {
-        
         return (
             <section className={ingrStyles.ingridiensContainer}>
                 <div className={ingrStyles.tabs}>
-                    {/* Компонент <Tab /> обрабатывает функцию, переданную в onClick={} и в качестве аргумента передаёт не event, а значение пропса value={} */}
-                    <Tab value="one" active={this.state.currentTab === 'one'} onClick={this.tabSwitch}>Булки</Tab>
-                    <Tab value="two" active={this.state.currentTab === 'two'} onClick={this.tabSwitch}>Соусы</Tab>
-                    <Tab value="three" active={this.state.currentTab === 'three'} onClick={this.tabSwitch}>Начинки</Tab>
+                    {/* Компонент <Tab /> в функцию из onClick={} в качестве аргумента передаёт не event, а значение пропса value={} */}
+                    <Tab value="one" active={currentTab === 'one'} onClick={setCurrentTab}>Булки</Tab>
+                    <Tab value="two" active={currentTab === 'two'} onClick={setCurrentTab}>Соусы</Tab>
+                    <Tab value="three" active={currentTab === 'three'} onClick={setCurrentTab}>Начинки</Tab>
                 </div>
                 <div className={ingrStyles.ingrDisplay + ' mt-10'}>
 
                     <div className={ingrStyles.ingrShowcase}>
                         <h3 className="text text_type_main-medium">Булки</h3>
                         <div className={ingrStyles.ingrList}>
-                            <CardList type={"bun"} ingridients={this.props.allIngridients} openModal={this.props.openModal} />
+                            <CardList type={"bun"} ingridients={allIngridients} openModal={openModal} />
                         </div>
                     </div>
 
                     <div className={ingrStyles.ingrShowcase}>
                         <h3 className="text text_type_main-medium">Соусы</h3>
                         <div className={ingrStyles.ingrList}>
-                            <CardList type={"sauce"} ingridients={this.props.allIngridients} openModal={this.props.openModal} />
+                            <CardList type={"sauce"} ingridients={allIngridients} openModal={openModal} />
                         </div>
                     </div>
 
                     <div className={ingrStyles.ingrShowcase}>
                         <h3 className="text text_type_main-medium">Начинки</h3>
                         <div className={ingrStyles.ingrList}>
-                            <CardList type={"main"} ingridients={this.props.allIngridients} openModal={this.props.openModal} />
+                            <CardList type={"main"} ingridients={allIngridients} openModal={openModal} />
                         </div>
                     </div>
 
@@ -70,7 +52,6 @@ class BurgerIngredients extends React.Component {
                 </div>
             </section>
         );
-    }
 }
 
 // в этом индентификаторе записан валидатор для объектов, находящихся внутри массива this.props.allIngridients
